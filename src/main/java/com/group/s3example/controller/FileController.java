18fb0e2bbf21e95d66b00ffbc5ec6a1dc1067092
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-09-10 15:03:47 +0700

    no msg

--- src/main/java/com/group/s3example/controller/FileController.java
+++ src/main/java/com/group/s3example/controller/FileController.java
@@ -2,6 +2,7 @@ package com.group.s3example.controller;
 
 import com.group.s3example.services.storage.FileStorageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Log4j2
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
@@ -26,5 +28,11 @@ public class FileController {
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path + "\"")
                 .body(new InputStreamResource(fileStorageService.downloadFile(path)));
+    }
+
+    @PostMapping("/webhook/status")
+    public ResponseEntity<?> webhook(@RequestBody Object request) {
+        log.info(request);
+        return ResponseEntity.ok().build();
     }
 }
